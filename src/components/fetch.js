8a03eebd5f0861ceb2eeb52e66d8d114904ea956
Hassan Al-najjar <ha77an19@gmail.com>
2020-03-18 10:38:54 +0200

    created folder components and add the fetch file #5

---
+++ src/components/fetch.js
@@ -0,0 +1,20 @@
+
+const checkResponse = response => {
+    if (response.status !== 200) {
+      console.log("error", response.status);
+      return;
+    }
+    return response.json();
+  };
+
+  export const getCity = (city) =>{
+
+   return fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=e0622d0b56604ababa2b4219302bd58d`)
+    .then(checkResponse)
+    .then(res => console.log(res))
+    .catch(err => {
+      console.log(err);
+    });
+    
+  }
+
