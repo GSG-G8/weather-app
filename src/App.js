61e2d517541734a1866c55e8899b90a31bdda0f1
Hassan Al-najjar <ha77an19@gmail.com>
2020-03-18 11:37:40 +0200

    create search input and button for search #7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import { getCity } from './components/fetch';
-
-
 export default class App extends React.Component {
 
+  state = {
+    result: 'gaza'
+  };
+  
+  resultInput = e => this.setState({result: e.target.value.trim().replace(/\s+/g, ' ')});
+
+  searchButton = () =>  getCity(this.state.result);
+
   render() {
+    // const {result} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1>hi </h1>
+          <input type='text' onChange={this.resultInput} /> <br/>
+          <button type='text' onClick={(this.searchButton)} > search </button>
         </header>
       </div>
     );
